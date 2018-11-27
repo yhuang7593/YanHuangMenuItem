c25edfc804b28375a9411a73af86f87121139568
BT_1N3_04@bktech.local <BT_1N3_04@bktech.local>
2018-11-27 08:41:34 -0500

    Initial commit

---
+++ src/com/company/Trio.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Trio {
+        Sandwich Sandwich;
+        Salad Salad;
+        Drink Drink;
+        public Trio
+        {
+            Trio Trio=new Trio(Sandwich, Salad, Drink);
+            this.Sandwich=Sandwich;
+            this.Salad=Salad;
+            this.Drink=Drink;
+        }
+        public getPrice() {
+            return Sandwich.getPrice();
+            return Salad.getPrice();
+            return Drink.getPrice();
+        }
+
+        public lowestOfThree() {
+            int first=Sandwich.getPrice();
+            int second=Salad.getPrice();
+            int third=Drink.getPrice();
+            int lowestOfThree
+        }
+
+
+
+
+
+    }
+}
